1c99f57eafc134bd041fd7509070c13e1fe0d950
Erik Xu <81636759@qq.com>
2022-05-24 02:47:38 +0800

    Add controller.

---
+++ src/WebApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello ErikXu!");
+        }
+    }
+}
